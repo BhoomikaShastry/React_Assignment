29a1fb02fa67675be38f3b0dc7c9757bdf6535a0
BhoomikaShastry <62304213+BhoomikaShastry@users.noreply.github.com>
2021-11-05 22:55:34 +0530

    Add files via upload

---
+++ Task Manager/src/components/TodoList.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from 'react';
+import CreateTask from '../modals/CreateTask'
+import Card from './Card';
+
+const TodoList = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+    
+    useEffect(() => {
+        let arr = localStorage.getItem("taskList")
+       
+        if(arr){
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
+
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+            <div className = "header text-center">
+                <h3>Task Manager</h3>
+                <a href="">
+                <button className = "btn btn-primary mt-2">Sign In</button></a>
+                </div>
+                </div>
+                
+                <button className = "btn btn-warning mt-2" onClick = {() => setModal(true)} >Create Task</button>
+                </nav>
+            <div className = "task-container">
+            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
+            </div>
+            <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
+        </>
+    );
+};
+
+export default TodoList;
